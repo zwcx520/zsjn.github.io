cb56a58d6c6c6a7c595122eff846a773758ad772
追吻辰星 <1837601513@qq.com>
2024-09-29 04:08:24 +0800

    Add files via upload

--- verification.js
+++ verification.js
@@ -7,7 +7,7 @@ $('#login-button').click(function (event) {
         $('form').fadeOut(2500);
         $('.wrapper').addClass('form-success');
         setTimeout(function () {
-            location.href = "https://zsjn.netlify.app/";
+            location.href = "https://zsxc.netlify.app/";
         }, 1000);
     } else {
         alert("用户名或密码不正确！");
