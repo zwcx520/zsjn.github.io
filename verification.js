ddfbe020f9f1d0f0e61f99689fb33eef76b2aac5
追吻辰星 <1837601513@qq.com>
2024-10-07 22:44:43 +0800

    Add files via upload

--- verification.js
+++ verification.js
@@ -1,7 +1,7 @@
 $('#login-button').click(function (event) {
     let userName = document.getElementById("userName").value;
     let pwd = document.getElementById("pwd").value;
-    if (userName == "公主" && pwd == "zyn") {
+    if (userName == "lcy" && pwd == "lcy") {
         
         event.preventDefault();
         $('form').fadeOut(2500);
